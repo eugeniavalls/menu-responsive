ff640e8619aa8a2d4587d95279ebc444406e4503
eugeniavalls <e.vallsalfaro@gmail.com>
2023-04-10 16:12:23 +0200

    apaño con git

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const toggleMenuElement = document.getElementById ('toggle-menu');
+const mainNavElement = document.getElementById ('main-nav');
+
+function handleClick (){
+    mainNavElement.classList.toggle ('hide');
+}
+
+toggleMenuElement.addEventListener('click' , handleClick);
